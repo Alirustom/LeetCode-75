453c1b605b6183c7b79cb28690c6c1ce9f4032a8
Rustam Ali <rustam@cwebconsultants.com>
2024-01-04 16:06:16 +0530

    check given element is vowels

--- leetcode.js
+++ leetcode.js
@@ -1,6 +1,16 @@
 let reverse_vowels = function(s){
    let arr = s.split();
-   let left=0; let right = arr.length-1;
-   let vowels = [a,e,i,o,u,A,E,I,O,U];
-   
+   let left = 0; let right = arr.length-1;
+   const vowels = [a,e,i,o,u,A,E,I,O,U];
+   while(left<right){
+    if(arr.indexOf(arr[left]) === -1){
+      left++;
+      continue;
+    }
+    if(arr.indexOf(arr[right]) === -1){
+      right++;
+      continue;
+    }
+   }
+
 }
