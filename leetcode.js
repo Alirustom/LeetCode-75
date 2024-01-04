3b7b1e3f9207e6ba56e460f1afb7a2d07a2517e6
Rustam Ali <rustam@cwebconsultants.com>
2024-01-04 18:45:46 +0530

    Reverse vowels of a string complete

--- leetcode.js
+++ leetcode.js
@@ -1,16 +1,24 @@
 let reverse_vowels = function(s){
-   let arr = s.split();
-   let left = 0; let right = arr.length-1;
-   const vowels = [a,e,i,o,u,A,E,I,O,U];
+   const arr = s.split("");
+   let left = 0; let right = arr.length;
+   const vowels = ['a','e','i','o','u','A','E','I','O','U'];
    while(left<right){
-    if(arr.indexOf(arr[left]) === -1){
+    if(vowels.indexOf(arr[left]) === -1){
       left++;
       continue;
     }
-    if(arr.indexOf(arr[right]) === -1){
-      right++;
+    if(vowels.indexOf(arr[right]) === -1){
+      right--;
       continue;
     }
+    const temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+    left++;
+    right--;
    }
-
+   return arr.join("");
 }
+res = reverse_vowels('hello');
+
+console.log(res);
